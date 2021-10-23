a03f0ffe0d7fa3936e47e678b19274a6d9dbbb8e
Kinson <kinsonkwong@lcad.edu>
2021-10-22 20:16:40 -0700

    Tried updating Teleport

--- Assets/Scripts/KinsonProject1/Teleport.cs
+++ Assets/Scripts/KinsonProject1/Teleport.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    [SerializeField] private CharacterController _controller = null;
     [SerializeField] private Transform teleportTarget;
     [SerializeField] private GameObject _player;
 
     public void OnTriggerEnter(Collider other)
     {
+        _controller.enabled = false;
         _player.transform.position = teleportTarget.transform.position;
+        _controller.enabled = true;
     }
+    
 }
