ec9bebfc6742994d04ee9eb92e82f5abcf73f568
Kinson <kinsonkwong@lcad.edu>
2021-10-28 18:59:21 -0700

    Working Title Screen

--- Assets/Scripts/KinsonProject1/TitleController.cs
+++ Assets/Scripts/KinsonProject1/TitleController.cs
@@ -2,38 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class TitleController : MonoBehaviour
 {
-    [SerializeField] GameObject mainMenu = null;
-    [SerializeField] GameObject optionMenu = null;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnClick_Play()
     {
         SceneManager.LoadScene("KinsonFirstGame");
-    }
-
-    public void OnClick_Option()
-    {
-        mainMenu.SetActive(false);
-        optionMenu.SetActive(true);
-    }
-
-    public void OnClick_Menu()
-    {
-        mainMenu.SetActive(true);
-        optionMenu.SetActive(false);
     }
 }
